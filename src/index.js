93e5791ddf0793fe81d9046af0b7604faab6eb46
ahyoung <yoo@yanadoocorp.com>
2022-12-21 14:53:51 +0900

    add mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
 const api = require('./api');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+// mongodb 연결
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((response) => {
+    console.log('Successfully connected to mongodb');
+  })
+  .catch((e) => {
+    console.error('error', e);
+  });
+
+const port = process.env.PORT || 4000;
 
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
@@ -31,6 +47,6 @@ router.get('/post', (ctx, next) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('heurm server is listening to port 4000');
+app.listen(port, () => {
+  console.log('heurm server is listening to port ' + port);
 });
