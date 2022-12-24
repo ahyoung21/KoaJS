1668205866354388d4726a30de4e63dc76debf1f
유아영 <ahyoung.db@gmail.com>
2022-12-24 13:21:44 +0900

    add auth, auth controller

--- src/index.js
+++ src/index.js
@@ -54,3 +54,12 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(port, () => {
   console.log('heurm server is listening to port ' + port);
 });
+
+const crypto = require('crypto');
+
+const password = 'abc123';
+const secret = 'MySecretKey1$1$234';
+
+const hashed = crypto.createHmac('sha256', secret).update(password).digest('hex');
+
+console.log(hashed);
